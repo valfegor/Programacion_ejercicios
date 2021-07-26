d3f313e9977bcd2141e17e2439cf4199db25dc71
Anthony Ahumada <locopala99@gmail.com>
2021-07-26 20:11:08 +0200

    adding

--- Login_utilizando_localstorage/login.js
+++ Login_utilizando_localstorage/login.js
@@ -1,2 +1,50 @@
 //este script es realizado realizar el inicio del login.
 
+//este script es la parte de inicio o pagina.
+
+const formulario = document.getElementById("form");
+const usuario = document.getElementById("username");
+const contrasto = document.getElementById("contraseña");
+
+console.log(contrasto)
+
+//obtengo los datos al dar clic al boton de subimit.
+formulario.addEventListener("submit",function(event) {
+
+    event.preventDefault();
+
+
+    //genero el array donde se van a guardar los datos
+    let nuevoarray = [{
+        nombre:usuario.value,
+        contraseña:contrasto.value,
+    }]
+
+    console.log(nuevoarray);
+
+    //localstorage solo recibe strings se debe convertir el arreglo de objetos a strings.
+
+    localStorage.setItem('usuario',JSON.stringify(nuevoarray));
+    
+    //
+
+    /*
+    if(nuevoarray[0].value!=localStorage.getItem('usuario')){
+        alert("no puyede ingresar");
+    }
+*/
+    
+
+    let prueba = localStorage.getItem('usuario');
+    console.log(prueba[0].nombre)
+
+    if(prueba[0].value!=prueba[0].value){
+        alert("no puede ingresar"); 
+        console.log(prueba[0].value);
+    }
+    if (prueba[0].value==prueba[0].value) {
+        alert("si puede");
+    }
+
+
+});
--- Login_utilizando_localstorage/nav.js
+++ Login_utilizando_localstorage/nav.js
@@ -1,48 +1,22 @@
-//este script es la parte de inicio o pagina.
+
 
 const formulario = document.getElementById("form");
-const usuario = document.getElementById("username");
-const contrasto = document.getElementById("contraseña");
-
-console.log(contrasto)
-
-//obtengo los datos al dar clic al boton de subimit.
-formulario.addEventListener("submit",function(event) {
-
-    event.preventDefault();
+const usuario = document.getElementById("username").value;
+const contrasto = document.getElementById("contraseña").value;
 
 
-    //genero el array donde se van a guardar los datos
-    let nuevoarray = [{
-        nombre:usuario.value,
-        contraseña:contrasto.value,
-    }]
-
-    console.log(nuevoarray);
-
-    //localstorage solo recibe strings se debe convertir el arreglo de objetos a strings.
-
-    localStorage.setItem('usuario',JSON.stringify(nuevoarray));
-    
-    //
-
-    /*
-    if(nuevoarray[0].value!=localStorage.getItem('usuario')){
-        alert("no puyede ingresar");
-    }
-*/
-
-
+formulario.addEventListener('submit',function(e){
     let prueba = localStorage.getItem('usuario');
-    console.log(prueba)
 
-    if(prueba[0].value!=prueba[0].value){
-        alert("no puede ingresar"); 
-        console.log(prueba[0].value);
+    e.preventDefault();
+
+    if(prueba==null){
+        return alert("registrese primero");
     }
-    if (prueba[0].value==prueba[0].value) {
-        alert("si puede");
+    else{
+        return alert("si puede ingresar");
     }
 
-
+    
+    
 });
